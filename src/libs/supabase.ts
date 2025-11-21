32498b47d0ca04d5e9b82bb85ea43a3513a5f5e1
Uditya <uditya204@gmail.com>
2025-11-21 11:09:48 +0530

    Replace Supabase URL and anon key with placeholders

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -4,8 +4,8 @@ import { createClient, processLock } from '@supabase/supabase-js';
 import { Database } from '@/database.types';
 import { AppState } from 'react-native';
 
-export const supabaseUrl = 'https://ykcpcgwzwrgvohhvrbrz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrY3BjZ3d6d3Jndm9oaHZyYnJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MTQ5ODcsImV4cCI6MjA2OTA5MDk4N30.z-DVQcPEnHd6Djkl9_H7xolbmsZu7AjnmTSsuB_rk-g';
+export const supabaseUrl = 'SUPABASE_URL';
+const supabaseAnonKey = 'SUPABASE_KEY';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
